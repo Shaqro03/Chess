d76ea4f8a68a6f1d63ba75308c7de5162c653b20
Shaqro <qtoyan03@gmail.com>
2024-04-13 18:04:10 +0400

    first

---
+++ ConsoleApp21/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp21;
+
+internal class Class1
+{
+    public enum tox
+    {
+        A, B, C, D, E, F, G, H
+    }
+}
---
+++ ConsoleApp21/Coord.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp21;
+
+internal struct Coord
+{
+    public char xCoord;
+    public int yCoord;
+    public Coord(char x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+}
+
+enum Figure
+{
+    K, //king
+    Q, //queen
+    B, //bishop
+    R, //rook
+    N   //knight(dzi)
+}
